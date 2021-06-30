c25fbffed023e900120c1d8a939a4ef338d94751
Pavel <useless.dinosaur@gmail.com>
2021-06-30 21:11:25 +0300

    Changing parallax in showreel level

--- src/levels/scripts/ShowreelChangeBg.gd
+++ src/levels/scripts/ShowreelChangeBg.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 export(Color) var bg_color = Color("72ae30")
+export(Array, NodePath) var disable_parallax
+export(Array, NodePath) var enable_parallax
 
 func _ready():
 	connect("body_entered", self, "on_body_entered")
@@ -8,3 +10,7 @@ func _ready():
 func on_body_entered(body):
 	if body.is_in_group("player"):
 		VisualServer.set_default_clear_color(bg_color)
+		for d in disable_parallax:
+			get_node(d).hide()
+		for e in enable_parallax:
+			get_node(e).show()
