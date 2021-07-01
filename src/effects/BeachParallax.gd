c81b26099e29077b26eb3cf765a5c256952a1929
Clayfisherman <61196761+Clayfisherman@users.noreply.github.com>
2021-07-01 19:32:08 +0400

    Promo gifs added

--- src/effects/BeachParallax.gd
+++ src/effects/BeachParallax.gd
@@ -1,8 +1,11 @@
 extends ParallaxBackground
 
-var MIST_VELOCITY = -16
-var MIST2_VELOCITY = -24
+
+var BG_VELOCITY = -3
+var MIST_VELOCITY = -5
+var MIST2_VELOCITY = -8
 
 func _process(delta):
+	$BG.motion_offset.x += delta * BG_VELOCITY
 	$MistLayer.motion_offset.x += delta * MIST_VELOCITY
 	$MistLayer2.motion_offset.x += delta * MIST2_VELOCITY
